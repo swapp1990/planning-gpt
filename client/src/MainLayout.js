81507878bc03435b8adf658df911688b05b5ae00
swapp <swapp19902@gmail.com>
2024-08-27 14:04:21 -0700

    mainlayout changes

--- client/src/MainLayout.js
+++ client/src/MainLayout.js
@@ -1,28 +1,32 @@
 import React, { useState, useEffect, useRef } from "react";
 import Header from "./components/Header";
 import BottomNavigation from "./components/BottomNavigation";
+import useViewportHeight from "./utils/useViewportHeight";
+
 export default function MainLayout({ children }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [windowHeight, setWindowHeight] = useState("100vh");
+
+  const viewportHeight = useViewportHeight();
 
   const handleToggleClick = () => {
     setSidebarOpen(!sidebarOpen);
   };
 
-  useEffect(() => {
-    const updateHeight = () => {
-      setWindowHeight(`${window.innerHeight}px`);
-    };
+  // useEffect(() => {
+  //   const updateHeight = () => {
+  //     setWindowHeight(`${window.innerHeight}px`);
+  //   };
 
-    window.addEventListener("resize", updateHeight);
-    updateHeight(); // Set initial height
+  //   window.addEventListener("resize", updateHeight);
+  //   updateHeight(); // Set initial height
 
-    return () => window.removeEventListener("resize", updateHeight);
-  }, []);
+  //   return () => window.removeEventListener("resize", updateHeight);
+  // }, []);
 
   return (
     <>
-      <div className="flex flex-col" style={{ height: windowHeight }}>
+      <div className="flex flex-col" style={{ height: `${viewportHeight}px` }}>
         {/* <Header onToggleSidebar={handleToggleClick} /> */}
         <header className="bg-blue-600 text-white shadow-md h-8 flex-shrink-0">
           Test Header
